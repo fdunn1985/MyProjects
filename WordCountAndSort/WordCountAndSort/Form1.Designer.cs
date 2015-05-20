043f9d9b22553bd012b456efac06a7b992eb3b10
Lee <fdunn1985@gmail.com>
2015-05-19 22:35:01 -0400

    WordCountAndSort project

---
+++ WordCountAndSort/WordCountAndSort/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WordCountAndSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.analyzeBtn = new System.Windows.Forms.Button();
+            this.resultsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(510, 293);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "";
+            // 
+            // analyzeBtn
+            // 
+            this.analyzeBtn.Location = new System.Drawing.Point(12, 311);
+            this.analyzeBtn.Name = "analyzeBtn";
+            this.analyzeBtn.Size = new System.Drawing.Size(71, 22);
+            this.analyzeBtn.TabIndex = 1;
+            this.analyzeBtn.Text = "Analyze";
+            this.analyzeBtn.UseVisualStyleBackColor = true;
+            this.analyzeBtn.Click += new System.EventHandler(this.analyzeBtn_Click);
+            // 
+            // resultsListBox
+            // 
+            this.resultsListBox.FormattingEnabled = true;
+            this.resultsListBox.Location = new System.Drawing.Point(579, 32);
+            this.resultsListBox.Name = "resultsListBox";
+            this.resultsListBox.Size = new System.Drawing.Size(192, 264);
+            this.resultsListBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(576, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 344);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultsListBox);
+            this.Controls.Add(this.analyzeBtn);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Word Count And Sort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.Button analyzeBtn;
+        private System.Windows.Forms.ListBox resultsListBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WordCountAndSort/WordCountAndSort/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordCountAndSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void analyzeBtn_Click(object sender, EventArgs e)
+        {
+            StartAnalysis(textBox.Text);
+        }
+
+        public void StartAnalysis(string text)
+        {
+            Dictionary<string, int> wordDictionary = new Dictionary<string, int>();
+
+            //remove punctuation
+            text = RemovePunctuation(text);
+            text.ToLower();
+
+            //alphabetize unique words
+            string[] textArr = text.Split(' ');
+            Array.Sort<string>(textArr);
+
+            //parse text (and count unique words)
+            wordDictionary = ParseAndCountUniqueWords(textArr);
+            
+            //Display Info
+            DisplayCountInfo(wordDictionary, textArr);
+        }
+
+        public string RemovePunctuation(string text)
+        {
+            return Regex.Replace(text, @"[^\w\s\-]", string.Empty);
+        }
+
+        private Dictionary<string, int> ParseAndCountUniqueWords(string[] textArr)
+        {
+            Dictionary<string, int> retDictionary = new Dictionary<string ,int>();
+
+            foreach (string word in textArr)
+            {
+                if (!retDictionary.ContainsKey(word))
+                {
+                    retDictionary.Add(word, 1);
+                }
+                else
+                {
+                    retDictionary[word] = retDictionary[word]+1;
+                }
+            }
+            return retDictionary;
+        }
+
+        public void DisplayCountInfo(Dictionary<string, int> wordDictionary, string[] textArr)
+        {
+            List<string> lines = new List<string>();
+            string wordCount = "" + textArr.Count();
+            string uniqueCount = "" + wordDictionary.Count();
+
+            lines.Add("Total Words: \t\t" + wordCount);
+            lines.Add("Total Unique Words: \t" + uniqueCount);
+            lines.Add(" ");
+
+            foreach (var word in wordDictionary)
+            {
+                string key = word.Key;
+                int value = word.Value;
+
+                if (key.Length < 9) //Formatting purposes only
+                    lines.Add(key + "\t\t" + value);
+                else
+                    lines.Add(key + "\t" + value);
+            }
+
+            resultsListBox.DataSource = lines;
+        }
+    }
+}
